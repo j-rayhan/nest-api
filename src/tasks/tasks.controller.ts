817358b2d940344728a80fe7848d92a70c860507
johir-rayhan <johir.rayhan01@gmail.com>
2022-02-15 21:49:58 +0600

    Create task function

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Task } from './tasks.model';
 import { TasksService } from './tasks.service';
 
@@ -9,5 +9,15 @@ export class TasksController {
   @Get()
   getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Task {
+    // createTask(@Body() body){ // We can also use this way
+    // console.log('title:-->', title);
+    return this.tasksService.createTask(title, description);
   }
 }
