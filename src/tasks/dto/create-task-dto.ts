2b16dcd285f527b60a3f71d4869b2fbb9e612c30
johir-rayhan <johir.rayhan01@gmail.com>
2022-02-16 23:33:35 +0600

    Implementing Validation Pipe

--- src/tasks/dto/create-task-dto.ts
+++ src/tasks/dto/create-task-dto.ts
@@ -1,4 +1,9 @@
+// Data Transfer Object (DTO)
+import { IsNotEmpty } from 'class-validator';
+
 export class CreateTaskDto {
+  @IsNotEmpty()
   title: string;
+  @IsNotEmpty()
   description: string;
 }
