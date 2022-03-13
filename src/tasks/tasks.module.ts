29089fad6c665af03df74f8ab4a730c2af878f4d
johir-rayhan <johir.rayhan01@gmail.com>
2022-03-13 08:52:00 +0600

    Remode Config Module test code

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -4,14 +4,9 @@ import { TasksRepository } from './tasks.repository';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 import { AuthModule } from '../auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [
-    ConfigModule,
-    TypeOrmModule.forFeature([TasksRepository]),
-    AuthModule,
-  ],
+  imports: [TypeOrmModule.forFeature([TasksRepository]), AuthModule],
   controllers: [TasksController],
   providers: [TasksService],
 })
