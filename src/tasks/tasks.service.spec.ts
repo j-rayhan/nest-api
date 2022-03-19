19b99efdd2845518200c92b32e0ed6b8fb41b467
johir-rayhan <johir.rayhan01@gmail.com>
2022-03-19 11:10:48 +0600

    WIP tasks testing....

---
+++ src/tasks/tasks.service.spec.ts
@@ -0,0 +1,43 @@
+import { Test } from '@nestjs/testing';
+import { TasksRepository } from './tasks.repository';
+import { TasksService } from './tasks.service';
+
+const mockTaskRepository = () => ({
+  getTasks: jest.fn(),
+});
+const mockUser = {
+  id: 'testId',
+  username: 'testUser',
+  password: 'testPassword',
+  tasks: [],
+};
+describe('Task Service Testing', () => {
+  let tasksService: TasksService;
+  let tasksRepository;
+
+  beforeEach(async () => {
+    // Initialize NestJS module and taskService and taskRepository
+    const module = await Test.createTestingModule({
+      providers: [
+        TasksService,
+        {
+          provide: TasksRepository,
+          useFactory: mockTaskRepository,
+        },
+      ],
+    }).compile();
+    tasksRepository = module.get(TasksRepository);
+    tasksService = module.get(TasksService);
+  });
+
+  describe('getTask', () => {
+    it('calls getTasks and return the results', async () => {
+      expect(tasksRepository.getTasks).not.toHaveBeenCalled();
+      // call getTasks, which should than call the repository's getTasks
+      tasksRepository.getTasks.mockResolvedValue('SomeValue');
+      const result = await tasksRepository.getTasks(null, mockUser);
+      expect(tasksRepository.getTasks).toHaveBeenCalled();
+      expect(result).toEqual('SomeValue');
+    });
+  });
+});
